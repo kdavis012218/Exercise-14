4a74152a64a56bbfa32d827c8bdb079f0f9b1f9e
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-25 20:42:13 -0500

    exercise 14

---
+++ Exercise 14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_14
+{
+    class Program
+    {
+
+        int input;
+        int squaresolution;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            Console.ReadLine();
+            
+
+
+
+            for (int squaresolution = input*input; counter++)
+            {
+                Console.WriteLine(counter * counter);
+                Console.WriteLine("Number :{0}, Square : {1}", counter, counter * counter);
+            }
+            Console.WriteLine("Would you like to continue?");
+            Console.ReadLine();
+
+
+        }
+    }
+}
